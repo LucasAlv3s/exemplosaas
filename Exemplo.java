e6877f2a960eb6e87407969dd5848b101aea80d0
Lucas Alves <lucasbiel40@gmail.com>
2022-08-15 23:39:45 +0000

    gravando a idade

--- Exemplo.java
+++ Exemplo.java
@@ -2,5 +2,7 @@ public class Exemplo{
 
     public static void main(String[] args) {
         System.out.println("Lucas");
+        int idade = 20;
+        System.out.println("tenho "+idade);
     }
 }
